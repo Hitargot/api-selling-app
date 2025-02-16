143f28cf90969e08ab1321998dcb668bbbf398ca
Omar <omarlareef51@gmail.com>
2025-02-17 05:57:39 +0800

    using git for heroku deployment

--- src/components/ServiceDetails.js
+++ src/components/ServiceDetails.js
@@ -1,15 +1,16 @@
 import { useParams } from "react-router-dom";
 import UserServices from "./UserServices";
-import ApiList from "./APIList";
+import ApiList from "./HomeList";
 
 const ServiceDetails = () => {
     const { id } = useParams();
-    
+
     return (
-      <div>
-        {id.startsWith("user") ? <UserServices serviceId={id} /> : <ApiList serviceId={id} />}
-      </div>
+        <div>
+            {/* Render UserServices if the ID corresponds to a user service, otherwise show ApiList */}
+            {id ? <UserServices serviceId={id} /> : <ApiList serviceId={id} />}
+        </div>
     );
-  };  
+};
 
 export default ServiceDetails;
