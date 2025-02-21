48029fcaeb2f84bc872528db56c60e16a872e924
Omar <omarlareef51@gmail.com>
2025-02-21 10:50:14 +0800

    using git for heroku deployment

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,7 @@ function Contact() {
       <p>Have questions? Feel free to reach out to us.</p>
       <div className="contact-info">
         <p><FaEnvelope className="contact-icon" /> Email: <a href="mailto:apismarketplace930@gmail.com">support@apimarket.com</a></p>
-        <p><FaPhone className="contact-icon" /> Phone: +1 (234) 567-890</p>
+        <p><FaPhone className="contact-icon" /> Phone: +1 (910) 734-6464</p>
       </div>
     </section>
   );
